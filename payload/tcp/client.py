d2b86057d23db08ab904b66bfe89275bf5c1964f
Jettson2 <thadelise@gmail.com>
2025-11-20 15:27:10 -0500

    code update video

--- payload/tcp/client.py
+++ payload/tcp/client.py
@@ -1,7 +1,7 @@
 import socket
 
-SERVER_ADDRESS = '0.0.0.0'
-SERVER_PORT = 5004 
+SERVER_ADDRESS = '10.0.0.1'
+SERVER_PORT = 5009
 
 if __name__ == "__main__":
 
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         print('Connected!')
 
         # Send text file line by line 
-        with open('test_image.jpeg','rb') as fh:
+        with open('test_video.mp4','rb') as fh:
             print('Sending file...')
             while True:
                 data = fh.read(1024)
@@ -19,5 +19,4 @@ if __name__ == "__main__":
                     break 
 
                 sock.sendall(data) # Send encoded bytes
-
     print('File transfer complete!')
